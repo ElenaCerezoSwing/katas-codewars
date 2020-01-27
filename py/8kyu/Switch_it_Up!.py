ed1fb3ca186e6b6033a66054945caba0af2b2e57
ElenaCerezoSwing <elena.cerezo.ibanez.contractor@bbva.com>
2020-01-27 20:35:19 +0100

    feat: adds switch it up

---
+++ py/8kyu/Switch_it_Up!.py
@@ -0,0 +1,5 @@
+def switch_it_up(number):
+  # We just create a list with the noun of numbers 
+    list_number_in_words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    # Then, we return its place in our list
+    return list_number_in_words[number]
